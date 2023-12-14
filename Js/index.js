85607fff354b22a72050b1933d8aaa834719bbf3
hungmanh02 <domanh462@gmail.com>
2023-12-14 15:53:43 +0700

    responsive menu navbar test

--- Js/index.js
+++ Js/index.js
@@ -1,10 +1,11 @@
 const userName = document.querySelector("p.username");
 const userLogin = JSON.parse(localStorage.getItem("userLogin"));
 const userRegister = JSON.parse(localStorage.getItem("userRegister"));
-const button = document.querySelector(".user button");
-
+const buttonLogout = document.querySelector(".user button");
+const buttonMenu = document.getElementById("menu-bar");
+const navbar = document.querySelector("header .navbar");
 userName.innerHTML = userLogin?.name || "";
-button.addEventListener("click", function (e) {
+buttonLogout.addEventListener("click", function (e) {
   e.preventDefault();
   localStorage.removeItem("userLogin");
   window.location.href = "register.html";
@@ -12,3 +13,8 @@ button.addEventListener("click", function (e) {
 if (userLogin === undefined) {
   window.location.href = "register.html";
 }
+buttonMenu.addEventListener("click", function (e) {
+  e.preventDefault();
+  navbar.style.left = "0";
+  navbar.innerHTML += `<a href="#!">${userLogin?.name}</a>`;
+});
