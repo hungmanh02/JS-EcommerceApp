ef9ab730ce5299d86ad77df52e76eac9bb55acd4
hungmanh02 <domanh462@gmail.com>
2023-12-26 11:27:49 +0700

    view data local Storage Carts

--- Js/cart.js
+++ Js/cart.js
@@ -1,3 +1,22 @@
 const userLogin = JSON.parse(localStorage.getItem("userLogin"));
 const userName = document.querySelector("p.username");
 userName.innerHTML = userLogin?.name || "";
+const cartTable = document.querySelector(".cart-detail table");
+const cartListTable = JSON.parse(localStorage.getItem("carts"));
+if (!cartListTable) {
+  cartTable.innerHTML += `<tr>
+                              <td colspan="5" style="text-align:center">Không có sản phẩm trong giỏ hàng</td>
+                          </tr>`;
+} else {
+  cartListTable.forEach(function (element) {
+    const { category, description, id, image, price, quantity, rating, title } =
+      element;
+    cartTable.innerHTML += `<tr>
+                                  <td>${title}</td>
+                                  <td><img src="${image}" alt="${title}"  style="width:50px"/></td>
+                                  <td>${price}</td>
+                                  <td>${quantity}</td>
+                                  <td>${category}</td>
+                              </tr>`;
+  });
+}
